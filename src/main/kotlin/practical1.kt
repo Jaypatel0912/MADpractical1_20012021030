52a55b5b19f4109407f59ed8ecc527007fdfa3bc
Jaypatel0912 <Jaypatel70460@gmail.com>
2022-08-05 12:17:58 +0530

    PRACTICAL1

--- src/main/kotlin/practical1.kt
+++ src/main/kotlin/practical1.kt
@@ -1,10 +1,22 @@
 
 fun main(){
     var x:Int=10
-    val y:Int=10
+    val y='J'
     val z=39.5f
+    val s="Hello World!"
+    var b:Boolean=true
+    var d:Double=45.23
+    var l:Long=23457874
+    var e:Short=-2
+    var byte:Byte=127
     println("Hello World!")
-    println("x = $x")
-    println("y = $y")
-    println("z = $z")
+    println("Integer = $x")
+    println("Float = $z")
+    println("Character = $y")
+    println("String = $s")
+    println("Boolean value = $b")
+    println("Double value = $d")
+    println("Long value = $l")
+    println("Short value = $e")
+    println("Byte value = $byte")
 }
