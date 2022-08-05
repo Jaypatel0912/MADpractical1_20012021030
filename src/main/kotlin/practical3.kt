6765afe09e036c2deffb11d0c91c0d4ff9fc90b1
Jaypatel0912 <Jaypatel70460@gmail.com>
2022-08-05 12:05:11 +0530

    PRACTICAL3

---
+++ src/main/kotlin/practical3.kt
@@ -0,0 +1,25 @@
+fun main(){
+    println("Student Enrollment no.:")
+    var x= readLine()!!.toInt()
+    println("Student Name:")
+    var n= readLine()!!
+    println("student Branch:")
+    var b= readLine()!!
+    println("student class:")
+    var c= readLine()!!
+    println("student College Name:")
+    var a= readLine()!!
+    println("student University Name:")
+    var u= readLine()!!
+    println("student Age:")
+    var d= readLine()!!.toInt()
+
+    println("Student Enrollment no.: $x")
+    println("Student Name: $n")
+    println("student Branch: $b")
+    println("student class: $c")
+    println("student College Name: $a")
+    println("student University Name: $u")
+    println("student Age: $d")
+
+}
